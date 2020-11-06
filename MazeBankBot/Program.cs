aee7655c2b30d21e78fab23dc1e317f7d189e1d2
xcgpseud <chris.evans92@hotmail.com>
2020-11-06 00:31:32 +0000

    First commit

---
+++ MazeBankBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MazeBankBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
